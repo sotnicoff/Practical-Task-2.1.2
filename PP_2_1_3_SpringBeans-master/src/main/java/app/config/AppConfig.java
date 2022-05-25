c073b1f9526fa19b8a5095493673327095669f18
Alexander <105540044+sotnicoff@users.noreply.github.com>
2022-05-25 12:19:28 +0300

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/config/AppConfig.java
@@ -0,0 +1,11 @@
+package app.config;
+
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+}
